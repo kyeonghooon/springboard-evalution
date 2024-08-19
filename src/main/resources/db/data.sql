e6300503588b11a0349f4f7919f07d61d48f4658
kyeonghooon <gibbon0510@gmail.com>
2024-08-19 14:07:15 +0900

    Docs : 샘플 데이터 추가

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,42 @@
-insert into board (title, content, author) values
-('제목1','내용1','홍길동'),
-('제목2','내용2','홍길동'),
-('제목3','내용3','홍길동'),
-('제목4','내용4','홍길동'),
-('제목5','내용5','홍길동');
+insert into board (title, content, author)
+ values ('제목1','내용1','홍길동');
+insert into board (title, content, author)
+ values ('제목2','내용2','홍길동');
+insert into board (title, content, author)
+ values ('제목3','내용3','홍길동');
+insert into board (title, content, author)
+ values ('제목4','내용4','홍길동');
+insert into board (title, content, author)
+ values ('제목5','내용5','홍길동');
+insert into board (title, content, author)
+ values ('제목6','내용6','홍길동');
+insert into board (title, content, author)
+ values ('제목7','내용7','홍길동');
+insert into board (title, content, author)
+ values ('제목8','내용8','홍길동');
+insert into board (title, content, author)
+ values ('제목9','내용9','홍길동');
+insert into board (title, content, author)
+ values ('제목10','내용10','홍길동');
+insert into board (title, content, author)
+ values ('제목11','내용11','홍길동');
+insert into board (title, content, author)
+ values ('제목12','내용12','홍길동');
+insert into board (title, content, author)
+ values ('제목13','내용13','홍길동');
+insert into board (title, content, author)
+ values ('제목14','내용14','홍길동');
+insert into board (title, content, author)
+ values ('제목15','내용15','홍길동');
+insert into board (title, content, author)
+ values ('제목16','내용16','홍길동');
+insert into board (title, content, author)
+ values ('제목17','내용17','홍길동');
+insert into board (title, content, author)
+ values ('제목18','내용18','홍길동');
+insert into board (title, content, author)
+ values ('제목19','내용19','홍길동');
+insert into board (title, content, author)
+ values ('제목20','내용20','홍길동');
+insert into board (title, content, author)
+ values ('제목21','내용21','홍길동');
